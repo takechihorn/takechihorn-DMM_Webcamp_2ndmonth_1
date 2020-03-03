0dc51dfd2bafbd12d384b59c572b2de8d0906047
takechihorn <takechihorn@gmail.com>
2020-03-03 17:40:40 +0900

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,62 @@
+class BooksController < ApplicationController
+	before_action :authenticate_user!
+	
+    def index
+        @book = Book.new
+        @books = Book.all
+	end
+	
+  def show
+	  @book = Book.find(params[:id])
+	  @user = @book.user
+	  @newbook = Book.new
+  end
+
+  def index
+	  @books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+	  @book = Book.new
+  end
+
+  def create
+	  @book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+	  @book.user_id = current_user.id #@bookの中にある、UserのidはhaLoginしているしているしているuserのidです。
+  	if @book.save#入力されたデータをdbに保存する。
+  		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
+  	else
+  		@books = Book.all
+  		render 'index'
+  	end
+  end
+
+  def edit
+	  @book = Book.find(params[:id])
+	  if current_user.id != @book.user_id
+		redirect_to books_path
+	  end
+	  
+  end
+
+
+
+  def update
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+  		redirect_to @book, notice: "successfully updated book!"
+  	else #if文でエラー発生時と正常時のリンク先を枝分かれにしている。
+  		render "edit"
+  	end
+  end
+
+  def destroy
+  	@book = Book.find(params[:id])
+  	@book.delete
+  	redirect_to books_path, notice: "successfully delete book!"
+  end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title ,:body)
+  end
+
+end
